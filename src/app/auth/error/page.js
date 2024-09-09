9be8a8228cdf5452a82ed9919fe4eaeea008daf3
taeseokOh <xotjr8052@gmail.com>
2024-09-09 18:31:11 +0900

    로그인 에러 페이지 수정

--- src/app/auth/error/page.js
+++ src/app/auth/error/page.js
@@ -1,21 +1,15 @@
 'use client';
 
 import { signIn } from "next-auth/react";
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+// import { useRouter } from 'next/navigation';
 
 export default function ErrorPage() {
-  const router = useRouter();
-  const { error } = router.query;
-
-  useEffect(() => {
-    console.log("OAuth Error:", error);
-  }, [error]);
+  // const router = useRouter();
 
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-red-50">
       <h1 className="text-3xl font-bold text-red-500">로그인 에러</h1>
-      <p className="text-red-400 mt-2">로그인 중 오류가 발생했습니다: {error}</p>
+      <p className="text-red-400 mt-2">로그인 중 오류가 발생했습니다</p>
       <button
         onClick={() => signIn()}
         className="mt-5 px-4 py-2 bg-blue-500 text-white rounded"
