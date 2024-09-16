3c8dc745407cc80b9585f41ffddb27be4b746735
taeseokOh <xotjr8052@gmail.com>
2024-09-16 23:33:36 +0900

    모달 컴포넌트) 외부 클릭시 닫히는 기능 추가

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,19 @@
-import React from 'react';
+'use client';
+
+import { useRef } from 'react';
 import ReactDOM from 'react-dom';
+import useOutsideClick from '../hooks/useOutsideClick';
 
 const Modal = ({ title, message, onConfirm, onCancel, type }) => {
+  const modalRef = useRef(null);
+
+  // 모달 외부 클릭 시 onCancel이 실행되도록 설정
+  useOutsideClick(modalRef, onCancel);
+
   return ReactDOM.createPortal(
     <div className="fixed inset-0 flex items-center justify-center p-4 bg-black bg-opacity-60 z-50">
       {/* 모바일에서도 좌우 여백을 추가하고, 컨테이너를 중앙에 배치 */}
-      <div className="bg-white w-full max-w-lg rounded-lg shadow-xl p-6 mx-4 relative">
+      <div ref={modalRef} className="bg-white w-full max-w-lg rounded-lg shadow-xl p-6 mx-4 relative">
         <h2 className="text-xl font-semibold mb-4 text-gray-900 text-center">{title}</h2>
         
         <div className="text-gray-700 mb-6">
