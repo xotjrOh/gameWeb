81e221eace884236c2d94c745eb27d8af4f83221
taeseokOh <xotjr8052@gmail.com>
2024-10-25 13:26:57 +0900

    셔플게임) 영상 시간 미세조정

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,29 +2,29 @@ export const videoDataList = {
     "끼리끼리": {
         videoId: "Gh-M6yHqpDc",
         clips: [
-            { start: 299, end: 302 },
-            { start: 302, end: 306 },
-            { start: 306, end: 309 },
-            { start: 309, end: 313 },
+            { start: 299, end: 302.3 },
+            { start: 302.3, end: 306 },
+            { start: 306, end: 309.5 },
+            { start: 309.5, end: 312.6 },
         ],
-        full: { start: 299, end: 313 },
+        full: { start: 299, end: 312.6 },
     },
     "도망가": {
         videoId: "9ric2y7vr9s",
         clips: [
-            { start: 35, end: 39 },
-            { start: 39, end: 43 },
-            { start: 43, end: 48 },
-            { start: 48, end: 50 },
+            { start: 35, end: 39.5 },
+            { start: 39.5, end: 43.8 },
+            { start: 43.8, end: 48.1 },
+            { start: 48.1, end: 50 },
         ],
         full: { start: 35, end: 50 },
     },
     "시카고": {
         videoId: "ZSXk6FzeWwo",
         clips: [
-            { start: 79, end: 83 },
-            { start: 83, end: 87 },
-            { start: 87, end: 90 },
+            { start: 79.8, end: 83.5 },
+            { start: 83.5, end: 86.9 },
+            { start: 86.9, end: 90 },
             { start: 90, end: 93 },
         ],
         full: { start: 79, end: 93 },
@@ -32,10 +32,10 @@ export const videoDataList = {
     "지각": {
         videoId: "aODhSiEI9qM",
         clips: [
-            { start: 46, end: 53 },
-            { start: 53, end: 59 },
-            { start: 59, end: 63 },
-            { start: 63, end: 69 },
+            { start: 46, end: 53.4 },
+            { start: 53.4, end: 58.6 },
+            { start: 58.6, end: 63.1 },
+            { start: 63.1, end: 69 },
         ],
         full: { start: 46, end: 69 },
     },
@@ -48,5 +48,24 @@ export const videoDataList = {
             { start: 30, end: 40 },
         ],
         full: { start: 0, end: 40 },
+    },
+    "기아차": {
+        videoId: "WwYl0ik2zDI",
+        clips: [
+            { start: 0, end: 9.3 },
+            { start: 9.3, end: 21 },
+            { start: 21, end: 37 },
+        ],
+        full: { start: 0, end: 37 },
+    },
+    "아싸": {
+        videoId: "MKiFTdaSgN0",
+        clips: [
+            { start: 118, end: 132.3 },
+            { start: 132.3, end: 157.1 },
+            { start: 157.1, end: 164.5 },
+            { start: 164.5, end: 180 },
+        ],
+        full: { start: 118, end: 180 },
     },
 };
