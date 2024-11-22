65551c53f8f64c6746c1f6adf74b5ff53980e0f2
taeseokOh <xotjr8052@gmail.com>
2024-11-23 07:20:59 +0900

    ts마이그레이션) middleware파일 완료

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
-export async function middleware(request) {
+export async function middleware(request: NextRequest) {
   // 토큰을 가져옵니다.
   const session = await getToken({ req: request });
   const { pathname } = request.nextUrl;
