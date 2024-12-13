1f5f500789013c0415d894818865cb751f60544f
taeseokOh <xotjr8052@gmail.com>
2024-12-13 18:11:24 +0900

    오래된 소켓 연결문제 해결; 방법 변경

--- src/app/api/version/route.ts
+++ src/app/api/version/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
-import packageJson from '../../../../package.json';
 
 export async function GET() {
-  const serverVersion = packageJson.version || '1.0.0'; // 기본값 설정
+  const serverVersion = process.env.SERVER_VERSION || '1.0.0';
   return NextResponse.json({ serverVersion });
 }
